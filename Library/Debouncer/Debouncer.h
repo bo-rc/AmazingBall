db01b68c49984a9d976528c2919973da8d0d4052
Bo Liu <bo-rc@acm.org>
2016-03-13 14:03:36 -0500

    adds void button_sample() to get samples in a single call

--- Library/Debouncer/Debouncer.h
+++ Library/Debouncer/Debouncer.h
@@ -7,8 +7,15 @@
 
 #ifndef DEBOUNCER_H
 #define	DEBOUNCER_H
+#include <p33Fxxxx.h>
+//do not change the order of the following 3 definitions
+#ifndef FCY
+#define FCY 12800000UL
+#endif
+#include <stdio.h>
+#include <libpic30.h>
 
-#include <stdint.h>
+#include "types.h"
 
 #ifdef	__cplusplus
 extern "C" {
@@ -20,7 +27,8 @@ extern "C" {
 #endif
 
 #define UNSTABLE          2
-#define UNCHANGED          3
+#define UNCHANGED         3
+#define DELAY_TIME_MS     2
 
 typedef struct {
     uint8_t samples[NUM_SAMPLES];
@@ -31,7 +39,9 @@ typedef struct {
 
 extern void button_init(Debouncer * button);
 
-extern void button_read(Debouncer * button, uint8_t Port_Reading);
+extern void button_sample(Debouncer * button, uint8_t Port_Reading); // get all samples in a single call
+
+extern void button_read(Debouncer * button, uint8_t Port_Reading); // get one sample per call
 
 extern uint8_t button_debounced(Debouncer* button);
 
